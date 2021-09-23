22242a194880e8b221d39fd0e1d8d203c6ca50bb
Luis <damluis_84@hotmail.com>
2021-09-23 14:12:44 +0200

    creado empleado

--- Empleado.java
+++ Empleado.java
@@ -10,5 +10,49 @@ package teis.dam2.git;
  * @author Luis_2
  */
 public class Empleado {
+    int edad ;
+    float altura;
+    float peso;
+    String nombre;
+
+    public Empleado(int edad, float altura, float peso, String nombre) {
+        this.edad = edad;
+        this.altura = altura;
+        this.peso = peso;
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public float getAltura() {
+        return altura;
+    }
+
+    public void setAltura(int altura) {
+        this.altura = altura;
+    }
+
+    public float getPeso() {
+        return peso;
+    }
+
+    public void setPeso(float peso) {
+        this.peso = peso;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
     
 }
---
+++ Maingit.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teis.dam2.git;
+
+/**
+ *
+ * @author Luis_2
+ */
+public class Maingit {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Empleado miEmpleado = new Empleado(1, 1.70f,67.4f,"Pedro");
+    }
+    
+}
