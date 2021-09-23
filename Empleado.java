c09527e1d704ae3d6648d822e8c11292f935b71e
Luis <damluis_84@hotmail.com>
2021-09-23 12:08:33 +0200

    incio del proyecto

---
+++ Empleado.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package teis.dam2.git;
+
+/**
+ *
+ * @author Luis_2
+ */
+public class Empleado {
+    
+}
